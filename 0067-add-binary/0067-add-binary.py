dc8665f8bf26cf788c8be324b8ed82031320899d
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-07-14 23:07:36 +0900

    Time: 0 ms (100%), Space: 18 MB (28.43%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,13 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        """
-        1. make a, b into decimal format
-        2. add a and b 
-        3. makt it into binary format
-        """
-        new_a=int(a,2)
-        new_b=int(b,2)
-        answer=new_a+new_b
-        #print(new_a,new_b)
-        answer=format(answer,"b")
-        return answer
+        temp1=int(a,2)
+        temp2=int(b,2)
+        #temp1=format(a,"b")
+        return format(temp1+temp2,"b")
