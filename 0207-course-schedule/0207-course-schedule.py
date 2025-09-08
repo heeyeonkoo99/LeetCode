65d58aec1c487c1add29caea4541c5b8819d90d6
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-09-08 14:26:03 -0500

    Time: 4 ms (67.94%), Space: 19.3 MB (51.24%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph=[[] for _ in range(numCourses)]
+
+        for a,b in prerequisites:
+            graph[a].append(b)
+        visited=[0] * numCourses
+
+        def dfs(node):
+            if visited[node]==1:
+                return False
+            if visited[node]==2:
+                return True
+            visited[node]=1
+            for i in graph[node]:
+                if not dfs(i):
+                    return False
+            visited[node]=2
+            return True
+
+        for i in range(numCourses):
+            if visited[i]==0 and not dfs(i):
+                return False
+        return True
+
