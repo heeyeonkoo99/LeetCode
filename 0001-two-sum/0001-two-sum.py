beddd5744812a1e5cb2d2b0ea55a636631405035
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-03 16:59:53 -0500

    Time: 1783 ms (16.77%), Space: 18.4 MB (78.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # nums.sort()를 하지않고 원래 인덱스에서 바꾸는게 필요함!
-        res=[]
-        if len(nums)==0:
+        if nums is None:
             return []
-        for i in range(len(nums)):
-            temp=target-nums[i]
-            print("temp",i, nums[i], temp)
-            if temp in nums and nums.index(temp)!=i:
-                res.append(i)
-                res.append(nums.index(temp))
-                return res
-    
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    print(nums[i],nums[j],target)
+                    return [i,j]
 
-
-       
 
         
