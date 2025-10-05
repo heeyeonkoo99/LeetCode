7804d37cd46e754246bd2569bdd6511b01755a8c
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-04 20:20:01 -0500

    Time: 3 ms (39.51%), Space: 17.6 MB (94.5%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,18 +1,21 @@
 from collections import Counter
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        d = Counter(s)
-        length = 0
-        odd_found = False
-        
-        for v in d.values():
-            if v % 2 == 0:
-                length += v
+        if not s:
+            return ""
+        length=0
+        cnt=Counter(s)
+        odd=False
+
+        for i in cnt.values():
+            if i%2==0:
+                length+=i
             else:
-                length += v - 1  # 짝수 부분만 사용
-                odd_found = True
-        
-        if odd_found:
-            length += 1  # 중앙에 하나 추가
-        
+                length+=i-1
+                odd=True
+        if odd:
+            length+=1
         return length
+
+        
