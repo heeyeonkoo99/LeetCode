cc1e5cd541599fc5e4cc6c2c61f4a27d0c7fc57a
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-06 15:28:32 -0500

    Time: 27 ms (72.56%), Space: 39.4 MB (28.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -1,18 +1,19 @@
-import collections
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from collections import deque
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        temp=collections.deque()
-        while head.next is not None:
-            temp.append(head.val)
+        d=deque()
+
+        while head:
+            d.append(head.val)
             head=head.next
-        temp.append(head.val)
-        while len(temp)>1:
-            if temp.pop()!=temp.popleft():
+        while len(d)>1:
+            if d.popleft()!=d.pop():
                 return False
+
         return True
-        
+
