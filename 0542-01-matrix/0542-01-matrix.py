5e7c110b736f03a14c8b4ba69058c54b735a3059
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-09 11:47:38 -0500

    Time: 115 ms (79.63%), Space: 20.7 MB (48.73%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,24 +2,23 @@ from collections import deque
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         q=deque()
-        res=[[-1]*(len(mat[0])) for _ in range( len(mat))]
+        m,n=len(mat),len(mat[0])
+        res=[[-1 for _ in range(n)] for _ in range(m)]
+        directions=[[-1,0],[0,-1],[0,1],[1,0]]
 
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
+        for i in range(m):
+            for j in range(n):
                 if mat[i][j]==0:
                     res[i][j]=0
-                    q.append((i,j))
-        directions=[(0,1),(1,0),(-1,0),(0,-1)]
+                    q.append([i,j])
 
         while q:
             x,y=q.popleft()
             for dx, dy in directions:
                 nx,ny=x+dx,y+dy
-                if 0<=nx<len(mat) and 0<=ny<len(mat[0]) and res[nx][ny]==-1:
+                if 0<=nx<m and 0<=ny<n and res[nx][ny]==-1:
+                    q.append([nx,ny])
                     res[nx][ny]=res[x][y]+1
-                    q.append((nx,ny))
         return res
-                    
 
-
-        
+     
