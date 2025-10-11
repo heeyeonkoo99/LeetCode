02b407a3cba02a86ff33b2ef8f4f1f54ccafb748
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-10 19:54:20 -0500

    Time: 4 ms (46.6%), Space: 18 MB (26.07%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,36 @@
+from collections import deque
+from typing import List
+
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh=set()
-        rotten=set()
+        if not grid:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        directions = [(-1,0), (1,0), (0,-1), (0,1)]
+        q = deque()
+        
+        # 1. 초기 썩은 오렌지 큐에 넣기
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    q.append((i, j, 0))  # x, y, time
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]==1:
-                    fresh.add(str(i)+str(j))
-                elif grid[i][j]==2:
-                    rotten.add(str(i)+str(j))
-        minutes=0
-        directions=[[0,1],[1,0],[-1,0],[0,-1]]
-
-        while fresh:
-            infected=set()
-            for orange in rotten:
-                i=int(orange[0])
-                j=int(orange[1])
-
-                for direction in directions:
-                    nextI=i+direction[0]
-                    nextJ=j+direction[1]
-
-                    if (str(nextI)+str(nextJ)) in fresh:
-                        fresh.remove(str(nextI)+str(nextJ))
-                        infected.add(str(nextI)+str(nextJ))
-            if not infected:
+        max_time = 0
+        
+        # 2. BFS
+        while q:
+            x, y, time = q.popleft()
+            max_time = max(max_time, time)
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                    grid[nx][ny] = 2
+                    q.append((nx, ny, time + 1))
+        
+        # 3. 신선한 오렌지 남아있으면 -1
+        for row in grid:
+            if 1 in row:
                 return -1
-            rotten=infected
-            minutes+=1
-        return minutes
+        
+        return max_time
