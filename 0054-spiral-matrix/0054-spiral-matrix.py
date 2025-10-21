efd4f4ed4c1ff47251b898a1894a47b45f9ebef3
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-21 13:42:48 -0500

    Time: 0 ms (100%), Space: 17.5 MB (98.44%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
-            return []
-        result=[]
-        left,right=0,len(matrix[0])-1
-        top,bottom=0, len(matrix)-1
-
+        top,bottom=0,len(matrix)-1
+        left,right=0, len(matrix[0])-1
+        res=[]
         while left<=right and top<=bottom:
-            for i in range(left,right+1):
-                result.append(matrix[top][i])
+
+            for j in range(left,right+1):
+                res.append(matrix[top][j])
             top+=1
 
+
             for i in range(top,bottom+1):
-                result.append(matrix[i][right])
+                res.append(matrix[i][right])
             right-=1
 
-            if top <= bottom:  # 아직 행이 남아 있다면
-                # 오른쪽 → 왼쪽
-                for i in range(right, left - 1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
+            if top<=bottom:
+                for j in range(right,left-1,-1):
+                    res.append(matrix[bottom][j])
+            bottom-=1
+            if left<=right:
+                for i in range(bottom,top-1,-1):
+                    res.append(matrix[i][left])
+            left+=1
+            
 
-            if left <= right:  # 아직 열이 남아 있다면
-                # 아래쪽 → 위쪽
-                for i in range(bottom, top - 1, -1):
-                    result.append(matrix[i][left])
-                left += 1
-        return result
-
+        print(res)
+        return res
+            
         
