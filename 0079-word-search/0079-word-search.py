4df6e25ed9830e35b0545cc1bc02b8e1e6906a54
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-21 17:39:35 -0500

    Time: 3192 ms (79.78%), Space: 17.4 MB (99.75%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        if not board:
-            return False
-        def dfs(board, word, i, j,k):
-            if k==len(word):
+        m,n=len(board),len(board[0])
+        
+        def dfs(i,j,index):
+            if index==len(word):
                 return True
-            if i<0 or i>=len(board) or j<0 or j>=len(board[0]) or board[i][j]!=word[k]:
-                return False
-            temp=board[i][j]
-            board[i][j]="#"
+            if 0<=i<m and 0<=j<n and board[i][j]==word[index]:
+                temp=board[i][j]
+                board[i][j]="#"
+                found=(
+                    dfs(i+1,j,index+1) or
+                    dfs(i-1,j,index+1) or
+                    dfs(i,j+1,index+1) or
+                    dfs(i,j-1,index+1) 
+                )
+                board[i][j]=temp
+                if found:
+                    return True
 
-            found = dfs(board, word,i-1,j,k+1) or dfs(board,word,i,j-1,k+1) or dfs(board, word,i+1,j,k+1) or dfs(board,word,i,j+1,k+1)
-            board[i][j]=temp
-            return found
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if dfs(board,word,i,j,0):
+            else:
+                return False
+        for i in range(m):
+            for j in range(n):
+                if dfs(i,j,0):
                     return True
         return False
-        
