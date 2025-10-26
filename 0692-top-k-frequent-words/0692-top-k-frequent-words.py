f12a3463e51e832cb8f770b66f01658d4fa0a1ce
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-25 22:46:46 -0500

    Time: 4 ms (20.74%), Space: 17.6 MB (98.16%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,8 +1,11 @@
 from collections import Counter
+import heapq
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         # counter써버리기..?
         count=Counter(words)
-        sorted_words=sorted(count.keys(), key=lambda w:(-count[w],w))
-        return sorted_words[:k]
+        heap=[(-freq,word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        res=[heapq.heappop(heap)[1] for _ in range(k)]
+        return res
         
