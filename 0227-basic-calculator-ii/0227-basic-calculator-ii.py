7465465d599a0836fcda5383cf4f46ecafe070b3
Heeyeon <68431716+heeyeonkoo99@users.noreply.github.com>
2025-10-31 21:50:35 -0500

    Time: 67 ms (61.26%), Space: 21.7 MB (47.13%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        if not s:
+            return 0
+        
+        stack = []
+        num = 0
+        sign = '+'
+        s = s.replace(' ', '')  # 공백 제거
+        
+        for i, c in enumerate(s):
+            if c.isdigit():
+                num = num * 10 + int(c)
+            
+            if c in '+-*/' or i == len(s) - 1:
+                if sign == '+':
+                    stack.append(num)
+                elif sign == '-':
+                    stack.append(-num)
+                elif sign == '*':
+                    stack.append(stack.pop() * num)
+                elif sign == '/':
+                    stack.append(int(stack.pop() / num))
+                
+                sign = c
+                num = 0
+        
+        return sum(stack)
