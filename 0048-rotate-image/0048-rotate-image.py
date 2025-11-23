a433462527eb26963ccd8630e4776f1b29dffb04
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-23 17:28:43 -0600

    Time: 0 ms (100%), Space: 17.8 MB (66.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -7,5 +7,5 @@ class Solution:
         for i in range(n):
             for j in range(i,n):
                 matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
-        for r in range(n):
-            matrix[r].reverse()
+        for i in range(n):
+            matrix[i].reverse()
