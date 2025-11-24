2bdd977664f322686dc0909ea21d99946a304dda
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-24 14:33:57 -0600

    Time: 20 ms (9.76%), Space: 21.4 MB (36.17%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,21 +2,28 @@ class MinStack:
 
     def __init__(self):
         self.stack=[]
+        self.minstack=[]
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        
+        if self.minstack:
+            self.minstack.append(min(val, self.minstack[-1]))
+        else:
+            self.minstack.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        self.minstack.pop()
+        return self.stack.pop()
         
 
     def top(self) -> int:
         return self.stack[-1]
+        
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.minstack[-1]
+
         
 
 
