57b9e4517d7645f332179b12150c98251fecd3db
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-23 19:40:52 -0600

    Time: 3 ms (95.9%), Space: 17.9 MB (79.59%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,21 @@
+from collections import Counter
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        s_to_t={}
+        t_to_s={}
+
+        for sc,tc in zip(s,t):
+            if sc in s_to_t:
+                if s_to_t[sc]!=tc:
+                    return False
+            else:
+                s_to_t[sc]=tc
+            if tc in t_to_s:
+                if t_to_s[tc] != sc:
+                    return False
+            else:
+                t_to_s[tc] = sc
+
+        return True
