b6f2156edb94d4d1c325daf00d749437ff20e96f
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-24 15:01:35 -0600

    Time: 4 ms (28.94%), Space: 19.2 MB (17.08%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -6,8 +6,8 @@ class Solution:
             if i not in operations:
                 stack.append(int(i))
             else:
-                a=stack.pop()
                 b=stack.pop()
+                a=stack.pop()
                 if i=="+":
                     stack.append(a+b)
                 elif i=="-":
