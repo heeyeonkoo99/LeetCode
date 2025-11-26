14cca3009a760bd0257636fcb4588b79cd71700b
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-25 19:59:00 -0600

    Time: 15 ms (40.6%), Space: 18.6 MB (11.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,4 @@
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict={}
-   
-        for i in s:
-            if i not in dict:
-                dict[i]=1
-            else:
-                dict[i]+=1
-
-        for i in t:
-            if i in dict and dict[i]!=0:
-                dict[i]-=1
-
-            else:
-                return False
-        return sum(dict.values())==0
-    
-
+        return sorted(s)==sorted(t)
         
