95cfa04899719d42635bdc54870ea90447d13332
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-11-28 11:55:36 -0600

    Time: 15 ms (5.72%), Space: 17.7 MB (84.61%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,23 +5,21 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyhead=ListNode(0)
-        tail=dummyhead
+        dummy=ListNode(0)
+        current=dummy
         carry=0
 
-        while l1 is not None or l2 is not None or carry!=0:
-            digit1=l1.val if l1 is not None else 0
-            digit2=l2.val if l2 is not None else 0
+        while l1 or l2 or carry:
+            val1=l1.val if l1 else 0
+            val2=l2.val if l2 else 0
+            total=val1+val2+carry
+            carry=total//10
+            current.next=ListNode(total%10)
+            current=current.next
 
-            sum=digit1+digit2+carry
-            digit=sum%10
-            carry=sum//10
+            if l1: l1=l1.next
+            if l2: l2=l2.next
+        
 
-            newNode=ListNode(digit)
-            tail.next=newNode
-            tail=tail.next
-
-            l1=l1.next if l1 is not None else None
-            l2=l2.next if l2 is not None else None
-        result=dummyhead.next
-        return result
+        return dummy.next
+        
