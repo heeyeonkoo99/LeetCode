7bd275fc3eab5ceb1c36e116b26847f5bfd906c4
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-12-12 23:47:04 -0600

    Time: 229 ms (90.89%), Space: 20.2 MB (56.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited=[[False for i in range(len(grid[0]))] for _ in range(len(grid))]
-        row,col=len(grid),len(grid[0])
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        m,n=len(grid),len(grid[0])
+        cnt=0
+        visited=[[False]*n for _ in range(m)]
+        directions=[[-1,0],[0,-1],[1,0],[0,1]]
 
         def dfs(r,c):
             visited[r][c]=True
-            
-            
-            for nx, ny in directions:
-                dx=nx+r
-                dy=ny+c
-               
 
-                if 0<=dx<row and 0<=dy<col and grid[dx][dy]=="1" and visited[dx][dy]==False:
-                    
+            for i in directions:
+                dx,dy=r+i[0],c+i[1]
+                if 0<=dx<m and 0<=dy<n and grid[dx][dy]=="1" and visited[dx][dy]==False:
                     dfs(dx,dy)
-            
-        count=0
-        for i in range(row):
-            for j in range(col):
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j]=="1" and visited[i][j]==False:
-                    count+=1
+                    cnt+=1
                     dfs(i,j)
-        return count
-
+        return cnt
         
