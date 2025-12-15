2c4e0d8ec890657375192c3e39b2102765246c5c
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-12-14 18:06:45 -0600

    Time: 187 ms (44.21%), Space: 32.9 MB (67.83%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -7,31 +7,31 @@ class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-
-        prev,slow,fast=None,head,head
+        slow,fast=head,head.next
         while fast and fast.next:
-            prev=slow
             slow=slow.next
             fast=fast.next.next
-        prev.next=None
+        mid=slow.next
+        slow.next=None
 
-        l1=self.sortList(head)
-        l2=self.sortList(slow)
+        left=self.sortList(head)
+        right=self.sortList(mid)
+        return self.merge(left,right)
+    def merge(self,l1:ListNode, l2:ListNode):
+        dummy=ListNode(0)
+        tail=dummy
 
-        return self.merge(l1,l2)
-
-    def merge(self,l1,l2):
-        dummy=tail=ListNode(0)
         while l1 and l2:
-            if l1.val<l2.val:
+            if l1.val< l2.val:
                 tail.next=l1
                 l1=l1.next
             else:
                 tail.next=l2
                 l2=l2.next
             tail=tail.next
-        tail.next=l1 or l2
-        return dummy.next
-
+        tail.next=l1 if l1 else l2
         
+
+
+        return dummy.next
         
