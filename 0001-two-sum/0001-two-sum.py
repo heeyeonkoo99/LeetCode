b11b3d457adaeba16029108a80ccd96ef48b68b3
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-12-21 23:50:46 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map={}
+        d=defaultdict(int)
+        for i,v in enumerate(nums):
+            d[v]=i
+        for i,v in enumerate(nums):
+            if target-v in nums and target-v!=v:
+                return [i, d[target-v]]
 
-        for i,num in enumerate(nums):
-            complement=target-num
-            if complement in map:
-                return [map[complement],i]
-            map[num]=i
-            
+
         
