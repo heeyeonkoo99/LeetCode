1a47f90878fb0cfe4ed1370976bc90fa49106647
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-12-31 16:07:07 -0600

    Time: 236 ms (76.34%), Space: 20 MB (73.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,13 +1,12 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
-        cnt=0
-        visited=[[False]*n for _ in range(m)]
         directions=[[-1,0],[0,-1],[1,0],[0,1]]
+        visited=[[False]*n for _ in range(m)]
+        cnt=0
 
         def dfs(r,c):
             visited[r][c]=True
-
             for i in directions:
                 dx,dy=r+i[0],c+i[1]
                 if 0<=dx<m and 0<=dy<n and grid[dx][dy]=="1" and visited[dx][dy]==False:
@@ -18,4 +17,6 @@ class Solution:
                     cnt+=1
                     dfs(i,j)
         return cnt
+        
+
         
