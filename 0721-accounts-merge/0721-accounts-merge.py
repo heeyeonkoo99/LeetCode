6c5719b7adc3b13f93a0a3441b8d718085f497e8
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2025-12-31 19:26:29 -0600

    Time: 35 ms (41.69%), Space: 22.7 MB (25.53%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -1,34 +1,29 @@
-from collections import defaultdict
+from collections import defaultdict 
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
-        visited_accounts=[False]*len(accounts)
-        email_accounts_map=defaultdict(list)
+        visited=[False]*len(accounts)
+        email_map=defaultdict(list)
         res=[]
 
-        #그래프 구축
-        for i,account in enumerate(accounts):
+
+        for i, account in enumerate(accounts):
             for j in range(1,len(account)):
                 email=account[j]
-                email_accounts_map[email].append(i)
-
-        #dfs함수
+                email_map[email].append(i)
         def dfs(i,emails):
-            if visited_accounts[i]:
+            if visited[i]:
                 return
-            visited_accounts[i]=True
+            visited[i]=True
             for j in range(1,len(accounts[i])):
                 email=accounts[i][j]
                 emails.add(email)
-                for neighbor in email_accounts_map[email]:
+                for neighbor in email_map[email]:
                     dfs(neighbor, emails)
-        #실행!
-        for i, account in enumerate(accounts):
-            if visited_accounts[i]:
+        for i,account in enumerate(accounts):
+            if visited[i]:
                 continue
-            name, emails=account[0],set()
-            dfs(i, emails)
+            name,emails=account[0],set()
+            dfs(i,emails)
             res.append([name]+sorted(emails))
 
         return res
-
-        
