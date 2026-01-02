df737e19a6266ebba1a3c074cdb5557abcafebce
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-01-01 18:42:10 -0600

    Time: 0 ms (100%), Space: 17.4 MB (88.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,16 +1,15 @@
+from itertools import combinations 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        map={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
         res=[]
-        map={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs", 8:"tuv",9:"wxyz"}
         def dfs(i,curr):
             if len(curr)==len(digits):
                 res.append(curr)
                 return
-            for alpha in map[int(digits[i])]:
-                dfs(i+1, curr+alpha)
+            for m in map[int(digits[i])]:
+                dfs(i+1,curr+m)
         if digits:
             dfs(0,"")
-        return res
-
-
         
+        return res
