97cc222ba35c60fa51670a8a308b5388afbd57b9
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-01-02 09:10:43 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,7 +3,8 @@ class Solution:
         if not intervals:
             return []
         res=[intervals[0]]
-        intervals.sort(key=labmda, x:x[0])
+        intervals.sort(key=lambda x:x[0])
+   
         for i in range(1,len(intervals)):
             if intervals[i][0]>res[-1][-1]:
                 res.append(intervals[i])
