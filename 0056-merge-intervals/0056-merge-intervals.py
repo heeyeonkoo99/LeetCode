aa0fb3908e7a3dd0b9a7294773586263a603760c
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-01-02 09:10:59 -0600

    Time: 6 ms (78.45%), Space: 20.5 MB (92.38%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,9 +2,9 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         if not intervals:
             return []
-        res=[intervals[0]]
+        
         intervals.sort(key=lambda x:x[0])
-   
+        res=[intervals[0]]
         for i in range(1,len(intervals)):
             if intervals[i][0]>res[-1][-1]:
                 res.append(intervals[i])
