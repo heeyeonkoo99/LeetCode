67df4f01d0e9b14dd76d4bbb1ca0ce29691c9344
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-01-06 18:51:18 -0600

    Time: 249 ms (49.43%), Space: 22.3 MB (33.2%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
-        directions=[[-1,0],[0,-1],[1,0],[0,1]]
         visited=[[False]*n for _ in range(m)]
-        cnt=0
+        directions=[[-1,0],[0,-1],[1,0],[0,1]]
 
-        def dfs(r,c):
-            visited[r][c]=True
-            for i in directions:
-                dx,dy=r+i[0],c+i[1]
+        def dfs(i,j):
+            visited[i][j]=True
+            for x,y in directions:
+                dx,dy=i+x,j+y
                 if 0<=dx<m and 0<=dy<n and grid[dx][dy]=="1" and visited[dx][dy]==False:
                     dfs(dx,dy)
+        cnt=0
         for i in range(m):
             for j in range(n):
                 if grid[i][j]=="1" and visited[i][j]==False:
-                    cnt+=1
                     dfs(i,j)
+                    cnt+=1
         return cnt
-        
 
         
