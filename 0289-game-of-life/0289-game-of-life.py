893b4d3b822ce94fa9dc209d3d5270fff4c5c23d
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-04-18 14:28:06 -0500

    Time: 0 ms (100%), Space: 19.3 MB (54.66%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -1,28 +1,32 @@
 class Solution:
     def gameOfLife(self, board: List[List[int]]) -> None:
-        m, n = len(board), len(board[0])
-        
-        # 8방향
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        m,n=len(board),len(board[0])
         dirs = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+    
+        for i in range(m):
+            for j in range(n):
+                live_negihbors=0
+                for dx,dy in dirs:
+                    ni,nj=i+dx,j+dy
+                    if 0<=ni<m and 0<=nj<n and board[ni][nj] in [1,3]:
+                        live_negihbors+=1
+                if board[i][j]==1 and (live_negihbors<2 or live_negihbors >3):
+                    board[i][j]=3 
+                if board[i][j]==0 and live_negihbors==3:
+                    board[i][j]=2
         
         for i in range(m):
             for j in range(n):
-                live_neighbors = 0
-                for dx, dy in dirs:
-                    ni, nj = i+dx, j+dy
-                    if 0 <= ni < m and 0 <= nj < n and board[ni][nj] in [1,3]:
-                        live_neighbors += 1
+                if board[i][j]==2:
+                    board[i][j]=1
+                elif board[i][j]==3:
+                    board[i][j]=0
+
+                    
+                    
+                    
                 
-                # rule 적용
-                if board[i][j] == 1 and (live_neighbors < 2 or live_neighbors > 3):
-                    board[i][j] = 3  # 1->0
-                if board[i][j] == 0 and live_neighbors == 3:
-                    board[i][j] = 2  # 0->1
-        
-        # 최종 상태로 바꾸기
-        for i in range(m):
-            for j in range(n):
-                if board[i][j] == 2:
-                    board[i][j] = 1
-                elif board[i][j] == 3:
-                    board[i][j] = 0
+                        
