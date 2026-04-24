ea82e956b50d6865fe901091e5403fefc7dc0cb8
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-04-23 22:51:34 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -8,16 +8,20 @@ class Solution:
         dummy=ListNode(0)
         curr=dummy
 
+
         while l1 and l2:
             if l1.val<=l2.val:
                 curr.next=l1
                 l1=l1.next
             else:
                 curr.next=l2
-                l2=l2.next
+                l2=l1.next
+            
             curr=curr.next
+           
+
         curr.next=l1 if l1 else l2
+        
+
 
         return dummy.next
-
-        
