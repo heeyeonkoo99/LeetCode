6a6b08e365b79e7f242e1d5ac8f6ccb25f46f0a9
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-04-30 23:29:29 -0500

    Time: 259 ms (30.3%), Space: 22.1 MB (32.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
-        visited=[[False]*n for _ in range(m)]
-        directions=[[-1,0],[0,-1],[1,0],[0,1]]
-
-        def dfs(i,j):
-            visited[i][j]=True
-            for x,y in directions:
-                dx,dy=i+x,j+y
-                if 0<=dx<m and 0<=dy<n and grid[dx][dy]=="1" and visited[dx][dy]==False:
-                    dfs(dx,dy)
         cnt=0
+        visited=[[False]*n for _ in range(m)]
+        def dfs(i,j):
+            
+            if i<0 or i>=m or j<0 or j>=n:
+                return
+            if visited[i][j]:
+                return 
+            if grid[i][j]=="0":
+                return
+            visited[i][j]=True
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            return 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=="1" and visited[i][j]==False:
-                    dfs(i,j)
+                if not visited[i][j] and grid[i][j]=="1":
                     cnt+=1
-        return cnt
+                    dfs(i,j)
 
+
+        return cnt
         
