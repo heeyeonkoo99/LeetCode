69cd3056f4d22b8a0ef330fc01c5bb13fa92b846
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-04-30 23:39:44 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,24 +6,30 @@ class Solution:
         if not board or not board[0]:
             return 0
         m,n=len(board),len(board[0])
+        visited=[[False]*n for _ in range(m)]
+        dir=[[0,1],[1,0],[-1,0],[0,-1]]
         def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n or board[i][j]!="O":
+            
+            if i<0 or i>=m or j<= or j>=n or board[i][j]!="O":
                 return
+            visied[i][j]=True
             board[i][j]="E"
-            for dx,dy in [[-1,0],[0,-1],[1,0],[0,1]]:
+            for dx,dy in dir:
                 dfs(i+dx,j+dy)
-                
         for i in range(m):
             dfs(i,0)
             dfs(i,n-1)
         for j in range(n):
             dfs(0,j)
             dfs(m-1,j)
-        
         for i in range(m):
             for j in range(n):
                 if board[i][j]=="O":
                     board[i][j]="X"
-                elif board[i][j]=="E":
+                if board[i][j]=="E":
                     board[i][j]="O"
+
+
+
+
         
