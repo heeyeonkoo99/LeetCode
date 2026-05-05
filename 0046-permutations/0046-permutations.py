059a76c49fef87a137839e847e33ab23c86eac98
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-05-05 14:07:22 -0500

    Time: 2 ms (37.69%), Space: 19.4 MB (66.13%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,12 +1,12 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res=[]
-        used=[False]*len(nums)
-
+        used=[False]*(len(nums))
         def dfs(path):
             if len(path)==len(nums):
                 res.append(path[:])
                 return
+            
             for i in range(len(nums)):
                 if used[i]:
                     continue
@@ -15,9 +15,6 @@ class Solution:
                 dfs(path)
                 path.pop()
                 used[i]=False
-        
-        
-        
         dfs([])
         return res
         
