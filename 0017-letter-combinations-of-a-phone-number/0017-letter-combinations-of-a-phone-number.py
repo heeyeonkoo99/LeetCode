cb673fab178f12fa68b9827e01e1662d1f135033
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-05-05 13:40:23 -0500

    Time: 0 ms (100%), Space: 19.3 MB (78.72%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,15 +2,13 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         map={2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
         res=[]
-        def dfs(i,curr):
-            if len(curr)==len(digits):
-                res.append(curr)
+        def dfs(i,path):
+            if i==len(digits):
+                res.append(path)
                 return
-            for m in map[int(digits[i])]:
-                dfs(i+1, curr+m)
-
-        if digits:
-            dfs(0,"")
+            for d in map[int(digits[i])]:
+                dfs(i+1,path+d)
+        dfs(0,"")
         return res
 
         
