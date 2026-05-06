ce7b5746123a74261046317c2a26f6342fc7c318
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-05-06 14:09:54 -0500

    Time: 46 ms (44.37%), Space: 24.1 MB (63.27%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -3,21 +3,18 @@ class Solution:
         total=0
         cur_max=0
         max_sum=nums[0]
-
         cur_min=0
         min_sum=nums[0]
 
         for x in nums:
             total+=x
 
-            cur_max=max(x, x+cur_max)
-            max_sum=max(max_sum,cur_max)
+            cur_max=max(x,cur_max+x)
+            max_sum=max(cur_max,max_sum)
 
-            cur_min=min(x, x+cur_min)
-            min_sum=min(min_sum,cur_min)
+            cur_min=min(x,cur_min+x)
+            min_sum=min(cur_min,min_sum) 
 
         if max_sum<0:
             return max_sum
         return max(total-min_sum, max_sum)
-
-        
