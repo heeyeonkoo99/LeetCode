aa636901c2b68f80a18e0f71352d320cea261ad7
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-05-11 16:16:41 -0500

    Time: 2 ms (75.41%), Space: 20.1 MB (43.78%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -4,5 +4,4 @@ class Solution:
         for i in range(len(triangle)-2,-1,-1):
             for j in range(len(triangle[i])):
                 dp[j]=triangle[i][j]+min(dp[j],dp[j+1])
-        return dp[0]
-        
+        return dp[0]        
