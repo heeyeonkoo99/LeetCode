892008f0df233c9bf62c9822d90eac8ce6247849
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-05-12 20:53:48 -0500

    Time: 87 ms (53.57%), Space: 32.6 MB (86.59%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -12,7 +12,7 @@ class Solution:
                     if i==0 or j==0:
                         dp[i][j]=1
                     else:
-                        dp[i][j]=min(
+                        dp[i][j]=1+min(
                             dp[i-1][j],
                             dp[i][j-1],
                             dp[i-1][j-1]
