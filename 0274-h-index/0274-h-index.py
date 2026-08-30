030889c13ac5928b0a44153c8fb90fba7e3a0951
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-08-30 16:17:26 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,10 +1,7 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        
+
         citations.sort(reverse=True)
         for i,v in enumerate(citations):
-            if v<i+1:
-                return i
-        
-
-        return len(citations)
+            if v>=i:
+                return i+1
