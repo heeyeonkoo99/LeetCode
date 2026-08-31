6ef9ca423751ecf25d3bbbad03357101140e45fd
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-08-31 14:14:50 -0500

    Time: 6 ms (46.18%), Space: 19.3 MB (58.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,14 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d={"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000,
-        "IV":4,"IX":9, "XL":40,"XC":90, "CD":400, "CM":900}
-        res=0
+        d={"I":1,"IV":4,"V":5,"IX":9,"X":10,
+         "XL":40,"L":50,"XC":90,"C":100,
+         "CD":400,"D":500,"CM":900,"M":1000}
+        ans=0
         i=0
+
         while i<len(s):
             if s[i:i+2] in d:
-                res+=d[s[i:i+2]]
+                ans+=d[s[i:i+2]]
                 i+=2
             elif s[i] in d:
-                res+=d[s[i]]
+                ans+=d[s[i]]
                 i+=1
-        return res
+        return ans
