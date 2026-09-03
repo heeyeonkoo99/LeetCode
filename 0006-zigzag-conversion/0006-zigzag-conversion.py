e0a2bcd26c71473051644850ef0575fae2591381
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-03 12:13:13 -0500

    Time: 9 ms (59.6%), Space: 19.1 MB (99.46%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,14 +2,14 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows==1 or numRows>=len(s):
             return s
-
-        going_down=False
-        row=[""] *numRows
         current_row=0
+        going_down=False
+        ans=[""]*numRows
         for i in s:
-            row[current_row]+=i
             if current_row==0 or current_row==numRows-1:
                 going_down=not going_down
+            ans[current_row]+=i
             current_row+=1 if going_down else -1
-        return "".join(row)
+        return "".join(ans)
+
         
