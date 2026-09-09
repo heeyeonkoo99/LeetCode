89dce40946dfe8a9b9c576378a99355554e3d1b7
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-09 12:11:20 -0500

    Time: 626 ms (61.71%), Space: 22.3 MB (54.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,7 +4,7 @@ class Solution:
         nums.sort()
         for i in range(len(nums)-2):
             left,right=i+1,len(nums)-1
-            while left<right and  nums[i]==nums[i+1]:
+            if i>0 and  nums[i]==nums[i-1]:
                 continue
             while left<right:
                 temp=nums[i]+nums[left]+nums[right]
@@ -13,11 +13,10 @@ class Solution:
                 elif temp>0:
                     right-=1
                 else:
-                    res.append([i,left,right])
-                    while left<right:
-                        if nums[right]==nums[right-1]:
+                    res.append([nums[i],nums[left],nums[right]])
+                    while left<right and nums[right]==nums[right-1]:
                             right-=1
-                        elif nums[left]==nums[left+1]:
+                    while left<right and nums[left]==nums[left+1]:
                             left+=1
                     left+=1
                     right-=1
