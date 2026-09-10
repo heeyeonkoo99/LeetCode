3577877fe9a5dd1ab2c84617b340b87aab8a1ed7
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-10 13:58:16 -0500

    Time: 11 ms (91.26%), Space: 19.8 MB (8.8%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -3,6 +3,8 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         a=Counter(ransomNote)
         b=Counter(magazine)
-        return True if a <= b else False
-
+        print(a,b)
+        if a<=b:
+            return True
+        return False
         
