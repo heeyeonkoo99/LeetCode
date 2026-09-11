fad89780815cc7fa4fbacfdc7a8c172b5b9ec6e7
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-11 17:44:38 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -2,15 +2,12 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         stack=[]
 
-        
         for p in path.split("/"):
-            if p=="" or p==".":
+            if p=="." or p=="/":
                 continue
-            elif p=="..":
+            if p=="..":
                 if stack:
                     stack.pop()
             else:
                 stack.append(p)
-            print(stack)
         return "/"+"/".join(stack)
-        
