0349ffc7c305acfa55edbefb721fd1160f475fbe
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-12 15:56:00 -0500

    Time: 0 ms (100%), Space: 19.2 MB (98.93%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -14,11 +14,10 @@ class Solution:
         for _ in range(left-1):
             prev=prev.next
         curr=prev.next
-    
+
         for _ in range(right-left):
             temp=curr.next
             curr.next=temp.next
             temp.next=prev.next
             prev.next=temp
         return dummy.next
-        
