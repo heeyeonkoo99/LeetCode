7837be23dd031f3dac3162a05617ef5d3b42b175
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-16 13:32:43 -0500

    Time: 0 ms (100%), Space: 19.5 MB (71.97%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -4,16 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def __init__(self):
-            self.prev=None
-    def flatten(self, root: Optional[TreeNode]) -> None:
+        self.prev=None
+    def flatten(self, root: TreeNode | None) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
-        
         if not root:
-            return 
+            return None
         self.flatten(root.right)
         self.flatten(root.left)
 
@@ -21,5 +21,4 @@ class Solution:
         root.left=None
 
         self.prev=root
-        
         
