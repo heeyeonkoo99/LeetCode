d7309561dec78141d667f2d8d8c2b0756f756023
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-17 21:47:35 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,27 +2,18 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m,n=len(grid),len(grid[0])
         cnt=0
-        visited=[[False]*n for _ in range(m)]
+
         def dfs(i,j):
-            
-            if i<0 or i>=m or j<0 or j>=n:
-                return
-            if visited[i][j]:
-                return 
-            if grid[i][j]=="0":
-                return
-            visited[i][j]=True
-            dfs(i+1,j)
-            dfs(i,j+1)
-            dfs(i-1,j)
-            dfs(i,j-1)
-            return 
+           
+            if i<m and j<n and grid[i][j]=="1":
+                dfs(i+1,j)
+                dfs(i,j+1)
+                dfs(i,j-1)
+                dfs(i-1,j)
+            return
         for i in range(m):
             for j in range(n):
-                if not visited[i][j] and grid[i][j]=="1":
-                    cnt+=1
+                if grid[i][j]=="1":
                     dfs(i,j)
-
-
+                    cnt+=1
         return cnt
-        
