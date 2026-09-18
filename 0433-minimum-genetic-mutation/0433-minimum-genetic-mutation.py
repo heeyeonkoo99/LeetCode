91cb044fa8a540d2795c837ceb1bccdbdfc6ded5
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-18 11:20:42 -0500

    Time: 0 ms (100%), Space: 19.4 MB (12.05%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -1,23 +1,22 @@
 class Solution:
-    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+    def minMutation(self, startGene: str, endGene: str, bank: list[str]) -> int:
         bank_set=set(bank)
-        if end not in bank_set:
+        if endGene not in bank_set:
             return -1
         genes=['A','C','G','T']
-        q=deque([(start,0)])
-        visited=set([start])
-
-        while q:
-            curr,steps=q.popleft()
-            if curr==end:
+        queue=deque([(startGene,0)])
+        visited=set([startGene])
+        while queue:
+            curr,steps=queue.popleft()
+            if curr==endGene:
                 return steps
             for i in range(len(curr)):
                 for g in genes:
-                    if g !=curr[i]:
-                        mutation=curr[:i]+g+curr[i+1:]
-                        if mutation in bank_set and mutation not in visited:
-                            visited.add(mutation)
-                            q.append((mutation, steps+1))
+                    if g!=curr[i]:
+                        temp=curr[:i]+g+curr[i+1:]
+                        if temp in bank_set and temp not in visited:
+                            visited.add(temp)
+                            queue.append((temp, steps+1))
         return -1
-        
+                    
         
