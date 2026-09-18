00c276fadb111db6bf96f1e26fdc2fe3e1946855
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-17 22:15:54 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -9,7 +9,7 @@ class Solution:
         visited=[[False]*n for _ in range(m)]
         dirs=[[-1,0],[0,-1],[1,0],[0,1]]
         def dfs(i,j):
-            if i<0 or i>=m or j<0 or j>=n:
+            if i<0 or i>=m or j<0 or j>=n or board[i][j]!="O":
                 return
             if visited[i][j]:
                 return
