503410ba827f886dd11a8e60afde9e2c218250e0
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-19 22:45:26 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,24 +1,22 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+    def exist(self, board: list[list[str]], word: str) -> bool:
         m,n=len(board),len(board[0])
-
-        def dfs(i,j,idx):
-            if idx==len(word):
-                return True
-            if i<0 or i>=m or j<0 or j>=n or board[i][j]!=word[idx]:
-                return False
-            temp=board[i][j]
-            board[i][j]="#"
-            found=(dfs(i,j+1,idx+1) or 
-            dfs(i+1,j,idx+1) or
-            dfs(i-1,j,idx+1) or
-            dfs(i,j-1,idx+1) )
-            board[i][j]=temp
-            if found:
-                return True
-            return False
+        visited=[[False]*n for _ in range(m)]
+        dirs=[[-1,0],[0,-1],[1,0],[0,1]]
+        def dfs(i,j):
+            if visited[i][j]:
+                return
+            if i<0 or i>=m or j<0 or j>=n:
+                return 
+            visited[i][j]=True
+            for dx, dy in dirs:
+                nx,ny=i+dx,j+dy
+                dfs(nx,ny)
+            return True
         for i in range(m):
             for j in range(n):
-                if dfs(i,j,0):
-                    return True
-        return False
+                if not dfs(i):
+                    return False
+        return True
+
+            
