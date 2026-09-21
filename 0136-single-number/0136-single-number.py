ef10abd7ea54e4aba754c33a500b0a62f5a4326f
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-21 16:46:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 from collections import Counter
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums: list[int]) -> int:
         a=Counter(nums)
-        for i,v in a.items():
-            if v==1:
+        for i in a:
+            if a[i]==2:
                 return i
