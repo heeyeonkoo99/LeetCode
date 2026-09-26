73025daaa9aa9210dbb48a5e6678386094fb15ad
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-26 14:04:31 -0500

    Time: 6 ms (92.78%), Space: 19.6 MB (37.25%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -14,7 +14,7 @@ class Solution:
             for j in range(1,n+1):
                 dp[i][j]=((
                     dp[i-1][j] and s1[i-1]==s3[i+j-1]
-                )and
+                )or
                 (dp[i][j-1] and s2[j-1]==s3[i+j-1]))
         return dp[m][n]
 
