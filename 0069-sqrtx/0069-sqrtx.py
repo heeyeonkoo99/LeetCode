aa17ff7687e2fade84f5f294c1e1d8b9d40d52ff
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-26 12:08:08 -0500

    Time: 6 ms (37.02%), Space: 19.2 MB (55.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -8,9 +8,9 @@ class Solution:
             if mid*mid==x:
                 return mid
             elif mid*mid<x:
-                l+=1
+                l=mid+1
             else:
-                r-=1
+                r=mid-1
         return r
 
         
