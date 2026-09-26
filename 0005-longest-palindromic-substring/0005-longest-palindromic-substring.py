6c4ffc281bc05cdae43051656d2f590a638ca328
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-26 13:56:08 -0500

    Time: 247 ms (71.68%), Space: 19.3 MB (37.35%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -11,9 +11,9 @@ class Solution:
         for i in range(len(s)):
             l1,r1=expand(i,i)
             l2,r2=expand(i,i+1)
-            if r1-l1>start-end:
+            if r1-l1>end-start:
                 start,end=l1,r1
-            if r2-l2>start-end:
+            if r2-l2>end-start:
                 start,end=l2,r2
         return s[start:end+1]
         
