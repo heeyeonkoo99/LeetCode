6153bc6a28f674d8c46a6aec5a5a8df87d3ca496
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-26 13:45:13 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,22 +1,21 @@
 class Solution:
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        m,n=len(grid),len(grid[0])
-
+    def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
+        m,n=len(obstacleGrid),len(obstacleGrid[0])
         dp=[[0]*n for _ in range(m)]
-        if dp[0][0]==1:
+        if obstacleGrid[0][0]==1:
             return 0
-        
+
         for i in range(1,m):
-            if dp[i][0]!=1:
-                dp[i][0]+=dp[i-1][0]
-
+            if obstacleGrid[i][0]!=1:
+                dp[i][0]=dp[i-1][0]+1
         for j in range(1,n):
-            if dp[0][j]!=1:
-                dp[0][j]+=dp[0][j-1]
-
-
+            if obstacleGrid[0][j]!=1:
+                dp[0][j]=dp[0][j-1]+1
         for i in range(1,m):
             for j in range(1,n):
-                if dp[i][j]!=1:
-                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])
-        return dp[-1][-1]
+                if obstacleGrid[i][j]!=1:
+                    dp[i][j]=max(dp[i][j-1],dp[i-1][j])+1
+
+        return dp[m-1][n-1]
+
+        
