c2f8c3f3b3c994b3dff1b64386869b49c5e9c44d
Hazel <68431716+heeyeonkoo99@users.noreply.github.com>
2026-09-26 13:55:07 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,8 +4,8 @@ class Solution:
             return ""
         def expand(l,r):
             while l>=0 and r<len(s) and s[l]==s[r]:
-                l+=1
-                r-=1
+                l-=1
+                r+=1
             return l+1,r-1
         start,end=0,0
         for i in range(len(s)):
